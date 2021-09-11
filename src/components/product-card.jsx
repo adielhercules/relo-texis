1ee00b826b9e6d2a4e628037d671da10c7b1b3a8
Adiel Hercules <jadher.11x2@gmail.com>
2021-09-10 22:44:39 -0600

    Remove brand

--- src/components/product-card.jsx
+++ src/components/product-card.jsx
@@ -50,7 +50,7 @@ export function ProductCard({ product, eager }) {
     <Link
       className={productCardStyle}
       to={slug}
-      aria-label={`View ${title} product page`}
+      aria-label={`Ver ${title}`}
     >
       {hasImage
         ? (
@@ -66,7 +66,6 @@ export function ProductCard({ product, eager }) {
         )
       }
       <div className={productDetailsStyle}>
-        <div className={productVendorStyle}>{vendor}</div>
         <h2 as="h2" className={productHeadingStyle}>
           {title}
         </h2>
